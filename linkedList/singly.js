4bd57adcaa4d3e6ceb25aa1af00993381af09076
Gerson Angel <gerson.angel@hotmail.es>
2021-01-19 08:37:10 -0500

    LinkedList - insert y getTheIndex

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -48,10 +48,37 @@ class MySinglyLinkedList {
     this.length++;
     return this;
   }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
 console.log(myLinkedList.append(2));
 console.log(myLinkedList.append(3));
 console.log(myLinkedList.append(4));
+console.log(myLinkedList.append(5));
 console.log(myLinkedList.prepend(0));
+console.log(myLinkedList.insert(2, 6));
+console.log(myLinkedList);
